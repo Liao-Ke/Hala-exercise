bcddbcee3f6152d2ffbc70b9271d3f5d719a4f3f
BarryBin <1815853581@qq.com>
2023-12-25 19:18:11 +0800

    解决跨域问题

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -5,6 +5,7 @@ import com.itheima.service.UserService;
 import entity.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpSession;
  */
 @Controller
 @ResponseBody
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
     @RequestMapping("/toMainPage")
     public String toMainPage() {
